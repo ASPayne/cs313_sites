b605bc8591504a9be87151d3710f01018b48f7fe
Andrew Payne <pay12008@byui.edu>
2019-05-01 13:37:55 -0600

    PHP_SELF color test

--- web/header.php
+++ web/header.php
@@ -1,18 +1,38 @@
 <header>
 	<?PHP 
 	$current_file = basename($_SERVER['PHP_SELF']);
-	 if($current_file == "home.php") {
+	 /*if($current_file == "home.php") {
 		$home_color = "Yellow";
 		} else {
 			$home_color = "Pink";
-		} ?>
+		}*/
+		$home_color = "Pink";
+		$about_us_color = "Pink";
+		$login_color = "Pink";
+
+
+	switch ($current_file){
+		case "home.php":
+			$home_color = "Yellow";
+			break;
+		case "about-us.php":
+			$about_us_color = "Yellow";
+			break;
+		case "login.php":
+			$login_color = "Yellow";
+			break;
+		default:
+			$home_color = "Pink";
+			$about_us_color = "Pink";
+			$login_color = "Pink";
+		}	 ?>
 
 
 	<h2>Think.</h2>
 		<ul>
 			<li><a style="background-color:<?PHP echo $home_color?>;" href="/home.php">Home</a></li>
-			<li><a href="/about-us.php">About Us</a></li>
-			<li><a href="/login.php">login</a></li>
+			<li><a style="background-color:<?PHP echo $about_us_color?>;" href="/about-us.php">About Us</a></li>
+			<li><a style="background-color:<?PHP echo $login_color?>;" href="/login.php">login</a></li>
 		</ul>
 
 		<H1><?PHP echo basename($_SERVER['PHP_SELF']); /* Returns The Current PHP File Name */ ?></H1>
