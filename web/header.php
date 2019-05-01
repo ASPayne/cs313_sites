9fceb50ab6bb6e1ace38685ca16f5fa69b0ed562
Andrew Payne <pay12008@byui.edu>
2019-05-01 13:29:21 -0600

    PHP_SELF color test

--- web/header.php
+++ web/header.php
@@ -6,9 +6,11 @@
 		} else {
 			$home_color = "Pink";
 		} ?>
+
+
 	<h2>Think.</h2>
 		<ul>
-			<li><a href="/home.php">Home</a></li>
+			<li><a style="background-color: <?PHP $home_color ?> ;" href="/home.php">Home</a></li>
 			<li><a href="/about-us.php">About Us</a></li>
 			<li><a href="/login.php">login</a></li>
 		</ul>
