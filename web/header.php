314c5a17054357a8265cc1077170eeeda7dadb01
Andrew Payne <pay12008@byui.edu>
2019-05-01 10:56:15 -0600

    php_self header test

--- web/header.php
+++ web/header.php
@@ -5,4 +5,6 @@
 			<li><a href="/about-us.php">About Us</a></li>
 			<li><a href="/login.php">login</a></li>
 		</ul>
+
+		<H1><?PHP echo basename($_SERVER['PHP_SELF']); /* Returns The Current PHP File Name */ ?></H1>
 	</header>
