3441364ea91777df88b0fd1a8857d66a8bc5a0dd
Andrew Payne <pay12008@byui.edu>
2019-05-01 13:20:08 -0600

    PHP_SELF test

--- web/header.php
+++ web/header.php
@@ -1,16 +1,15 @@
 <header>
 	<?PHP
 	$current_file = basename($_SERVER['PHP_SELF']);
-
+	 if($current_file == 'home.php') {
+		$home_color = "Yellow"
+		} else {
+			$home_color = "Pink"
+		}
 	?>
 	<h2>Think.</h2>
 		<ul>
-			<li><div style="background-color:Blue;"><a style="<?PHP if($current_file == 'home.php') {
-echo "background-color:Yellow;"
-} else {
-	echo "background-color:Pink;"
-}?>
-href="/home.php">Home</a></div></li>
+			<li><div style="background-color:Blue;"><a style="background-color: $home_color ;" href="/home.php">Home</a></div></li>
 			<li><a href="/about-us.php">About Us</a></li>
 			<li><a href="/login.php">login</a></li>
 		</ul>
