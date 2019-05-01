debb2665065881cea467b30bfdd231b514f8e0d9
Andrew Payne <pay12008@byui.edu>
2019-05-01 11:04:56 -0600

    PHP_SELF test

--- web/header.php
+++ web/header.php
@@ -5,7 +5,7 @@
 	?>
 	<h2>Think.</h2>
 		<ul>
-			<li color="blue"><a href="/home.php">Home</a></li>
+			<li><a color="blue" href="/home.php">Home</a></li>
 			<li><a href="/about-us.php">About Us</a></li>
 			<li><a href="/login.php">login</a></li>
 		</ul>
