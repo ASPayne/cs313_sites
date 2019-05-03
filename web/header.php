ede1a2369472f5cd8de83682a0cd5209450ef760
Andrew Payne <pay12008@byui.edu>
2019-05-02 22:16:26 -0600

    navbar update

--- web/header.php
+++ web/header.php
@@ -7,21 +7,29 @@
 	$about_us_color = "inherit";
 	$login_color = "inherit";
 
+	$home_active = false;
+	$about_active = false;
+	$login_active = false;
+
 	switch ($current_file) {
 		case "home.php":
 			$home_color = "Teal";
+			$home_active = true;
 			break;
 		case "about-us.php":
 			$about_us_color = "Teal";
+			$home_active = true;
 			break;
 		case "login.php":
 			$login_color = "Teal";
+			$home_active = true;
 			break;
 		default:
 			$home_color = "inherit";
 			$about_us_color = "inherit";
 			$login_color = "inherit";
 	}
+	true;
 	?>
 
 
@@ -30,7 +38,7 @@
 		<h2>Think.</h2>
 
 		<ul class=navbar>
-			<li class=navitem><a style="background-color:<?PHP echo $home_color ?>;" href="/home.php">Home</a></li>
+			<li class=navitem><a <?PHP if ($home_active) {echo "class=active;";} ?> href="/home.php">Home</a></li>
 			<li class=navitem><a style="background-color:<?PHP echo $about_us_color ?>;" href="/about-us.php">About Us</a></li>
 			<li class=navitem><a style="background-color:<?PHP echo $login_color ?>;" href="/login.php">login</a></li>
 		</ul>
