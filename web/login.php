82176ca29100bb157c77cf6813de3180c1d7a3f2
Andrew Payne <pay12008@byui.edu>
2019-05-02 23:33:02 -0600

    login test

--- web/login.php
+++ web/login.php
@@ -3,40 +3,41 @@
 //session_start();
 include 'session.php';
 
+if (isset($_POST['user'])){
 $_SESSION['user'] = $_POST['user'];
+}
 
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 	<title>Login</title>
-	
+
 </head>
+
 <body>
-<?php
-include 'header.php';
-?>
-<main>
+	<?php
+	include 'header.php';
+	?>
+	<main>
+
+		<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
+			Name: <input type="text" name="user"><br>
+			<button type="submit">Temp Login as Admin</button>
+		</form>
 
 
+		<p>
+		</p>
+		<?PHP echo $_SESSION['user']; ?>
 
-
-			
-<form action = "<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
-Name: <input type="text" name="user"><br>
-<button type="submit">Temp Login as Admin</button>
-</form>
-
-
-<p>
-</p>
-<?PHP echo $_SESSION['user']; ?>
-
-<a href="/home.php">Log In as Administrator</a>
-<a href="/home.php">Log In as Tester</a>
-</main>
+		<a href="/home.php">Log In as Administrator</a>
+		<a href="/home.php">Log In as Tester</a>
+	</main>
 </body>
+
 </html>
