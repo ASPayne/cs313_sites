af9ef521fb6964136b345f0a09156502eb827954
Andrew Payne <pay12008@byui.edu>
2019-05-02 21:59:23 -0600

    navbar update

--- web/header.php
+++ web/header.php
@@ -29,10 +29,10 @@
 
 		<h2>Think.</h2>
 
-		<div class=navbar>
-			<a class=navitem style="background-color:<?PHP echo $home_color ?>;" href="/home.php">Home</a>
-			<a class=navitem style="background-color:<?PHP echo $about_us_color ?>;" href="/about-us.php">About Us</a>
-			<a class=navitem style="background-color:<?PHP echo $login_color ?>;" href="/login.php">login</a>
-		</div>
+		<ul class=navbar>
+			<li><a class=navitem style="background-color:<?PHP echo $home_color ?>;" href="/home.php">Home</a></li>
+			<li><a class=navitem style="background-color:<?PHP echo $about_us_color ?>;" href="/about-us.php">About Us</a></li>
+			<li><a class=navitem style="background-color:<?PHP echo $login_color ?>;" href="/login.php">login</a></li>
+		</ul>
 	</div>
 </header>
