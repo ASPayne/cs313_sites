0b7cbf23efd2a983dae044b4e94e21ab30b8b246
Andrew Payne <pay12008@byui.edu>
2019-05-04 11:12:53 -0600

    link to main.css

--- web/home.php
+++ web/home.php
@@ -10,7 +10,8 @@ include 'session.php';
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="style/main.css">
 	<title>Home</title>
 </head>
 
