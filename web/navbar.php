6c248de146a6073d6d78a63098cdd6f8ccd0320a
Andrew Payne <pay12008@byui.edu>
2019-05-04 11:36:36 -0600

    add ; to navbar php

--- web/navbar.php
+++ web/navbar.php
@@ -2,7 +2,7 @@
 <?PHP
 $current_file = basename($_SERVER['PHP_SELF']);
 
-$NavBarActive = array("Home" => "", "About" => "", "Pages" => "", "login" => "");
+$NavBarActive = array("Home" => " ", "About" => " ", "Pages" => " ", "login" => " ");
 /*echo "Peter is " . $age['Peter'] . " years old.";*/
 
 switch ($current_file) {
@@ -20,27 +20,17 @@ switch ($current_file) {
         $NavBarActive["Pages"] = " active";
         break;
     default:
-        $NavBarActive = array("Home" => "", "About" => "", "Pages" => "", "login" => "");
+        $NavBarActive = array("Home" => " ", "About" => " ", "Pages" => " ", "login" => " ");
 }
 ?>
 
 <ul class="navbar" style="top: 0px; position: relative;">
-    <li class="navitem<?PHP echo $NavBarActive[" Home"] /*if ($home_active) {echo " active" ;}*/ ?>">
+    <li class="navitem<?PHP echo $NavBarActive[" Home"]; /*if ($home_active) {echo " active" ;}*/ ?>">
         <a href="/home.php">Home</a></li>
-    <li class="navitem<?PHP echo $NavBarActive[" About"] /*if ($about_active) { echo " active" ;}*/ ?>">
+    <li class="navitem<?PHP echo $NavBarActive[" About"]; /*if ($about_active) { echo " active" ;}*/ ?>">
         <a href="/about-us.php">About Us</a></li>
-    <li class="navitem<?PHP echo $NavBarActive[" Pages"] /*if ($login_active) {echo " active" ;}*/ ?>">
+    <li class="navitem<?PHP echo $NavBarActive[" Pages"]; /*if ($login_active) {echo " active" ;}*/ ?>">
         <a href="/pages.php">Pages</a></li>
-    <li style="float:right; background-color:teal" class="navitem<?PHP echo $NavBarActive[" Login"] /*if ($login_active) {echo " active" ;}*/ ?>">
+    <li style="float:right; background-color:teal" class="navitem<?PHP echo $NavBarActive[" Login"]; /*if ($login_active) {echo " active" ;}*/ ?>">
         <a href="/login.php">Login</a></li>
 </ul>
-<?PHP
-
-
-
-
-
-
-
-
-?>
