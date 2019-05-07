cf419068ed919837205fa0dfb93deea3765850c5
aspayne <pay12008@byui.edu>
2019-05-07 15:44:31 -0600

    activeweek3test

---
+++ web/activityweek3.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+
+
+<form>
+
+<input type="text" placeholder="Name">
+<input tyoe="text" placeholder="Email">
+
+<input type="radio" value="CS"  name="Major">
+<input type="radio" value="WDD" name="Major">
+<input type="radio" value="CIT" name="Major">
+<input type="radio" value="CE"  name="Major">
+
+<textarea name="Comments" rows="5" cols="40"> </textarea>
+
+</form>
+    
+</body>
+</html>
