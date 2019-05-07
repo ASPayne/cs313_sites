309fa0e46132712f60f09da4592f0e4c5b809471
aspayne <pay12008@byui.edu>
2019-05-07 16:08:43 -0600

    team active week 3

--- web/TeamActivities/Week03/activityweek3.php
+++ web/TeamActivities/Week03/activityweek3.php
@@ -4,13 +4,29 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" type="text/css" href="../../style/main.css">
     <title>Document</title>
 </head>
 <body>
 
+<?PHP
+if ($_SERVER["REQUEST_METHOD"] == "POST")
+{
+$NAME = $_POST["Name"];
+$EMAIL = $_POST["Email"];
+$MAJOR = $_POST["Major"];
+$COMMENT = $_POST["Comments"];
+
+//$VISITED  = $_POST["Visited"];
+
+}
+
+include '../../header.php';
+?>
 
 
-<form>
+<form method="post" action="<?PHP htmlspecialchars($_SERVER["PHP_SELF"]); ?>" > 
 
 <input type="text" placeholder="Name">
 <br>
@@ -26,6 +42,17 @@
 <textarea name="Comments" rows="5" cols="40" placeholder="Enter Comments Here"></textarea>
 
 </form>
-    
+<br>
+
+<p>
+NAME = <?PHP echo $NAME ?> <br>
+EMAIL = <?PHP echo $EMAIL ?> <br>
+MAJOR = <?PHP echo $MAJOR ?>  <br>
+COMMENT = <?PHP echo $COMMENTS ?> <br>
+<br>
+<!-- visited = <?PHP //echo $VISITED ?> -->
+</p>
+
+
 </body>
 </html>
