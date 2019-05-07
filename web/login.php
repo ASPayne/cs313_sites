e28f5d5a8e4f5937676d69fc040de51d61bcccbb
aspayne <pay12008@byui.edu>
2019-05-07 14:13:37 -0600

    login testing

--- web/login.php
+++ web/login.php
@@ -37,11 +37,11 @@ function login_as_tester()
         <h2> This Page Is Under Construction. The following are attempts to make a login process work. </h2>
         <hr>
         <br>
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'].login_as_admin()); ?>" method="post">
+        <form action="<?php login_as_admin(); ?>" method="post">
             <button type="submit">Login as Admin</button>
         </form>
 
-        <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF'].login_as_tester()); ?>" method="post">
+        <form action="<?php login_as_tester(); ?>" method="post">
             <button type="submit">Login as Tester</button>
         </form>
 
