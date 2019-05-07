1fab4909d48a7a69151f5008876a400af5b0d313
aspayne <pay12008@byui.edu>
2019-05-07 15:15:10 -0600

    script cleanup

--- web/login.php
+++ web/login.php
@@ -64,14 +64,15 @@ if (isset($_POST['user'])) {
                 }
             }
             ?>
-        </div> <!-- /container -->
+        </div>
 
         <div class="container">
 
             <form class="form-signin" role="form" action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);
                                                             ?>" method="post">
                 <h4 class="form-signin-heading"><?php echo $msg; ?></h4>
-                <input type="text" class="form-control" name="username" placeholder="username = tutorialspoint" required autofocus></br>
+                <input type="text" class="form-control" name="username" placeholder="username = tutorialspoint" required autofocus>
+                <br>
                 <input type="password" class="form-control" name="password" placeholder="password = 1234" required>
                 <button class="btn btn-lg btn-primary btn-block" type="submit" name="login">Login</button>
             </form>
