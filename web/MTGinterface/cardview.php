05701f4f29620a8c5e73221478352b60258b2e8d
aspayne <pay12008@byui.edu>
2019-06-02 13:42:49 -0600

    got composer working

--- web/MTGinterface/cardview.php
+++ web/MTGinterface/cardview.php
@@ -1,11 +1,12 @@
-
 <?PHP
 session_start();
-#include '../../vendor/autoload.php';
+require '../../vendor/autoload.php';
+use mtgsdk\Card;
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -16,38 +17,36 @@ session_start();
 
 <?PHP
 include '../header.php';
-include 'cardquery.php';
-echo $GLOBALS['url'];
-echo "<br>" . (sizeof($card)-1);
-//echo findbyname('Fog');
-echo $card['imageUrl'];
-var_dump($cards);
+//include 'cardquery.php';
+//echo $card['imageUrl'];
+//var_dump($cards);
 
 
-//$newcard = Card::find(386616);
-echo '<br>';
-var_dump($newcard);
+$card = Card::find(386617);
+//echo $card->imageUrl;
+
+echo '<br><br><br>';
+
+
+//var_dump($card);
 ?>
 
 <body>
-    
-<div class="cardview">    
-<img src="<?PHP echo $card['imageUrl'];?>" alt="<?PHP echo $card['name'];?>" style="float:left;margin-right:15px;">
-<h1> <?PHP echo $card['name'];?> </h1>
-<br/>
-<p> <?PHP echo $card['text'];?> </p>
-<br/><br/><br/><br/>
-
-<br>
-<form class="form" action="addCardToDeck.php" method="POST">
-
-<input type="button" name="action" value="add to deck2">
-
-</form>
-
-</div>
-
-
+    <div class="cardview">
+        <img src="<?PHP echo $card->imageUrl; ?>" alt="<?PHP echo $card->name; ?>" style="float:left;margin-right:15px;">
+        <h1>
+            <?PHP echo $card->name; ?>
+        </h1>
+        <br />
+        <p>
+            <?PHP echo $card->originalText; ?>
+        </p>
+        <br /><br /><br /><br />
+        <br>
+        <form class="form" action="addCardToDeck.php" method="POST">
+            <input type="button" name="action" value="add to deck2">
+        </form>
+    </div>
 </body>
 
 <?PHP
